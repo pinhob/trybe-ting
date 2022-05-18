cae6218c575efdf62fcdda31f97e14e49a047642
Bruno Pinho <brunopinho@outlook.com>
2022-05-18 19:13:17 -0300

    feat: faz req. 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,25 @@
+from collections import deque
+
+
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        # adaptado de: https://www.codingem.com/what-is-a-fifo-queue/
+        self.queue = deque([])
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.queue.popleft()
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        if index < 0 or index > (self.__len__() - 1):
+            raise IndexError
+        else:
+            print(self.queue)
+            return self.queue[index]
