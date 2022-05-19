3b2f5b118a6cf2f089a9aac9e14272ff10404cd8
Bruno Pinho <brunopinho@outlook.com>
2022-05-19 18:35:15 -0300

    feat: faz req. 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -40,4 +40,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        print(instance.queue[position], file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
