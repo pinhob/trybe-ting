879eed5526df7e393c051b6bf67e4543450cb157
Bruno Pinho <brunopinho@outlook.com>
2022-05-19 18:23:18 -0300

    feat: faz req. 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,33 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
+def search_for_file(path_file, instance):
+    file_is_new = True
+
+    if len(instance.queue) >= 1:
+        for item in instance.queue:
+            if item["nome_do_arquivo"] == path_file:
+                file_is_new = False
+
+    return file_is_new
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_content = txt_importer(path_file)
+
+    file_infos = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_content),
+        "linhas_do_arquivo": file_content,
+    }
+
+    file_is_new = search_for_file(path_file, instance)
+
+    if file_is_new:
+        instance.enqueue(file_infos)
+
+    print(file_infos, file=sys.stdout)
 
 
 def remove(instance):
