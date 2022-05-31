fc5d73dd7e24cfabc9d00cb72bd6317441307ba2
Bruno Pinho <brunopinho@outlook.com>
2022-05-30 21:24:49 -0300

    feat: feito requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,7 +2,7 @@ from queue import Queue
 
 
 def search_for_word_in_lines(word, instance):
-    search_result = {"file_name": "", "lines_with_word": []}
+    search_result = {"file_name": "", "lines_with_word": [], "lines": []}
 
     for item in instance.queue:
         for line in item["linhas_do_arquivo"]:
@@ -13,6 +13,8 @@ def search_for_word_in_lines(word, instance):
                 search_result["lines_with_word"].append(
                     (item["linhas_do_arquivo"].index(line)) + 1
                 )
+
+                search_result["lines"].append(line)
 
     return search_result
 
@@ -33,6 +35,8 @@ def exists_word(word, instance):
         for number in file_has_word["lines_with_word"]:
             search_result[0]["ocorrencias"].append({"linha": number})
 
+        print(file_has_word["lines"])
+
         return search_result
     else:
         return []
@@ -40,6 +44,28 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    file_has_word = search_for_word_in_lines(word, instance)
+
+    if len(file_has_word["lines_with_word"]) > 0:
+        search_result = [
+            {
+                "palavra": word,
+                "arquivo": file_has_word["file_name"],
+                "ocorrencias": [],
+            }
+        ]
+
+        for index, number in enumerate(file_has_word["lines_with_word"]):
+            conteudo = file_has_word["lines"][index]
+
+            search_result[0]["ocorrencias"].append({
+                "linha": number,
+                "conteudo": conteudo
+            })
+
+        return search_result
+    else:
+        return []
 
 
 if __name__ == "__main__":
